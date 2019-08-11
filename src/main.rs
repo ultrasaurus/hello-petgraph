c73260733c8dbd96ef7610901c39c671d4780b2f
Sarah Allen <sarah@ultrasaurus.com>
2019-08-11 16:36:47 -0700

    read/parse some commands from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+use petgraph::Graph;
+use std::io;
+use std::borrow::Cow;
+
+#[derive(Debug, Clone)]
+enum Command<'a> {
+    Exit,
+    Unknown,
+    Help,
+    Add(Vec<&'a str>),
+    Connect
+}
+
+fn print_help() {
+    println!("-----");
+    println!("Available Commands:");
+    println!("add/a - add node");
+    println!("help/h - displays this help");
+    println!("exit/x - quits the program");
+    println!("-----");
+}
+
+
+fn parse_line<'a, 'b>(input: &'a str) -> Cow<'b, Command> {
+    let mut iter = input.split_whitespace();
+    let command_str = iter.next().unwrap_or("");
+    let args:Vec<&str> = iter.collect();
+    let command = match command_str {
+        "add" | "a" => Command::Add(args.to_owned()),
+        "connect" | "c" => Command::Connect,
+        "exit" | "x" => Command::Exit,
+        "h" => Command::Help,
+        _ => {
+            println!("unknown command: {}", command_str);
+            Command::Unknown
+        }
+    };
+    return Cow::Owned(command);
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("hello graph!");
+    //let mut g = Graph::<&str, &str>::new();
+
+    print_help();
+    // let mut command;
+    // let mut args:Vec<&str>;
+    let mut input = String::new();
+    loop {
+        input.clear();
+        io::stdin().read_line(&mut input).expect("read from stdin");
+        let command = parse_line(&input.as_str()).into_owned();
+
+        match command {
+            Command::Exit => break,
+            Command::Help => print_help(),
+            Command::Add(args) => {
+                for s in args {
+                    println!("adding {}", s);
+                    //g.add_node(s);
+                }
+            },
+            Command::Connect => (),
+            Command::Unknown => ()
+        }
+    }
 }
