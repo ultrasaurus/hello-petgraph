b029a71abb66f60ceb74540e6a9ab3a6542a664e
Sarah Allen <sarah@ultrasaurus.com>
2019-08-17 11:17:56 -0700

    use impl to assoc functions with Command enum

--- src/main.rs
+++ src/main.rs
@@ -11,46 +11,46 @@ enum Command<'a> {
     Connect
 }
 
-fn print_help() {
-    println!("-----");
-    println!("Available Commands:");
-    println!("add/a - add node");
-    println!("help/h - displays this help");
-    println!("exit/x - quits the program");
-    println!("-----");
-}
-
-
-fn parse_line<'a, 'b>(input: &'a str) -> Cow<'b, Command> {
-    let mut iter = input.split_whitespace();
-    let command_str = iter.next().unwrap_or("");
-    let args:Vec<&str> = iter.collect();
-    let command = match command_str {
-        "add" | "a" => Command::Add(args.to_owned()),
-        "connect" | "c" => Command::Connect,
-        "exit" | "x" => Command::Exit,
-        "h" => Command::Help,
-        _ => {
-            println!("unknown command: {}", command_str);
-            Command::Unknown
-        }
-    };
-    return Cow::Owned(command);
+impl Command<'_> {
+    pub fn print_help() {
+        println!("-----");
+        println!("Available Commands:");
+        println!("add/a - add node");
+        println!("help/h - displays this help");
+        println!("exit/x - quits the program");
+        println!("-----");
+    }
+    pub fn parse_line<'a, 'b>(input: &'a str) -> Cow<'b, Command> {
+        let mut iter = input.split_whitespace();
+        let command_str = iter.next().unwrap_or("");
+        let args:Vec<&str> = iter.collect();
+        let command = match command_str {
+            "add" | "a" => Command::Add(args.to_owned()),
+            "connect" | "c" => Command::Connect,
+            "exit" | "x" => Command::Exit,
+            "h" => Command::Help,
+            _ => {
+                println!("unknown command: {}", command_str);
+                Command::Unknown
+            }
+        };
+        return Cow::Owned(command);
+    }
 }
 
 fn main() {
     println!("hello graph!");
     let mut g = Graph::<String, String>::new();
 
-    print_help();
+    Command::print_help();
     loop {
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("read from stdin");
-        let command = parse_line(&input.as_str()).into_owned();
+        let command = Command::parse_line(&input.as_str()).into_owned();
 
         match command {
             Command::Exit => break,
-            Command::Help => print_help(),
+            Command::Help => Command::print_help(),
             Command::Add(args) => {
                 for s in args {
                     println!("adding {}", s);
